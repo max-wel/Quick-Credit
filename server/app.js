be7c333a0976a0b91dc05781cdcf303ef50dc828
Your NameSneakymax <maximusekeh@gmail.com>
2019-05-15 13:29:47 +0100

    Fix swagger docs

--- server/app.js
+++ server/app.js
@@ -1,18 +1,20 @@
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
+import path from 'path';
 import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import router from './routes/index';
-import swaggerDocument from './swagger.json';
 
 const app = express();
 const port = process.env.PORT || 4000;
+const swaggerDoc = YAML.load(path.join(process.cwd(), './server/docs/docs.yaml'));
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/api/v1', router);
 
 app.use((req, res) => {
