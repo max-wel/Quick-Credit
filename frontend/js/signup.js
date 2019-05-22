faed57ab6470d54cea869c682210cc4209c8b529
Your NameSneakymax <maximusekeh@gmail.com>
2019-05-22 21:26:18 +0100

    feat(UI): Consume signup endpoint
    [Finishes #166195818]

---
+++ frontend/js/signup.js
@@ -0,0 +1,51 @@
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const address = document.querySelector('#address');
+const password = document.querySelector('#password');
+const form = document.querySelector('form');
+const modal = document.querySelector('.modal');
+const signupBtn = document.querySelector('.form-btn');
+const apiUrl = 'https://quick-credit-max.herokuapp.com/api/v1/auth/signup';
+
+const createModal = ({ error }) => {
+  modal.innerHTML = `<p>${error}</p>`;
+  modal.style.display = 'block';
+  setTimeout(() => {
+    modal.style.display = 'none';
+  }, 5000);
+};
+
+const signup = (e) => {
+  e.preventDefault();
+  signupBtn.textContent = 'Loading...';
+  // populate post body
+
+  const body = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    address: address.value,
+    password: password.value,
+  };
+  fetch(apiUrl, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+    .then(res => res.json())
+    .then((response) => {
+      signupBtn.textContent = 'Sign Up';
+      if (response.status !== 201) {
+        createModal(response);
+        return;
+      }
+      window.localStorage.setItem('token', response.data.token);
+      window.location.href = 'client-dashboard.html';
+    })
+    .catch(error => console.log(error));
+};
+form.addEventListener('submit', signup);
